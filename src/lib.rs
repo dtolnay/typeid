b0b30dfb7b1abf7bd51788b291da8230649eb306
David Tolnay <dtolnay@gmail.com>
2024-05-13 23:10:56 -0700

    Add crosslink icons to top of rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! [![github]](https://github.com/dtolnay/typeid)&ensp;[![crates-io]](https://crates.io/crates/typeid)&ensp;[![docs-rs]](https://docs.rs/typeid)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+//!
 //! # Const `TypeId` and non-'static `TypeId`
 //!
 //! <br>
